156375a41e76f726d5826bbef988649a64487990
Ali-10011 <ali.hussnainbabar@gmail.com>
2023-03-20 14:00:35 +0500

    Add Drawer Skeleton

--- fast_accounts/lib/views/home.dart
+++ fast_accounts/lib/views/home.dart
@@ -16,12 +16,12 @@ class _HomePageState extends State<HomePage> {
     AlertDialog alert = AlertDialog(
       content: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children:const [
-         CircularProgressIndicator(),
-             Text("Processing...."),
+        children: const [
+          CircularProgressIndicator(),
+          Text("Processing...."),
         ],
       ),
-    );  
+    );
     showDialog(
       barrierDismissible: false,
       context: context,
@@ -60,14 +60,196 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          showLoaderDialog(context);
-          _generateToken();
-        },
-        icon: const Icon(Icons.token_outlined),
-        label: const Text('Get Token'),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Dashboard'),
+        ),
+        drawer: Drawer(
+          child: ListView(children: [
+            const ListTile(
+              title: Text('FAST ACCOUNTS'),
+            ),
+            ListTile(
+              leading: const Icon(Icons.dashboard),
+              title: const Text('Dashboard'),
+              onTap: () {},
+            ),
+            ExpansionTile(
+              title: const Text("Bank"),
+              leading: const Icon(Icons.account_balance), //add icon
+              childrenPadding:
+                  const EdgeInsets.only(left: 60), //children padding
+              children: [
+                ListTile(
+                  title: const Text("Account Balances"),
+                  onTap: () {
+                    //action on press
+                  },
+                ),
+
+                ListTile(
+                  title: const Text("Bank Payments"),
+                  onTap: () {
+                    //action on press
+                  },
+                ),
+                ListTile(
+                  title: const Text("Bank Receipts"),
+                  onTap: () {
+                    //action on press
+                  },
+                ),
+                ListTile(
+                  title: const Text("Transfers"),
+                  onTap: () {
+                    //action on press
+                  },
+                ),
+                ListTile(
+                  title: const Text("Reconcile"),
+                  onTap: () {
+                    //action on press
+                  },
+                ),
+
+                //more child menu
+              ],
+            ),
+            ExpansionTile(
+              title: const Text("Sales"),
+              leading: const Icon(Icons.attach_money), //add icon
+              childrenPadding:
+                  const EdgeInsets.only(left: 60), //children padding
+              children: [
+                ListTile(
+                  title: const Text("Invoices"),
+                  onTap: () {
+                    //action on press
+                  },
+                ),
+
+                ListTile(
+                  title: const Text("Receipts"),
+                  onTap: () {
+                    //action on press
+                  },
+                ),
+                ListTile(
+                  title: const Text("Sales All"),
+                  onTap: () {
+                    //action on press
+                  },
+                ),
+                ListTile(
+                  title: const Text("Transfers"),
+                  onTap: () {
+                    //action on press
+                  },
+                ),
+                ListTile(
+                  title: const Text("Orders"),
+                  onTap: () {
+                    //action on press
+                  },
+                ),
+                ListTile(
+                  title: const Text("Customers"),
+                  onTap: () {
+                    //action on press
+                  },
+                ),
+
+                //more child menu
+              ],
+            ),
+            ExpansionTile(
+              title: const Text("Purchases"),
+              leading: const Icon(Icons.money_outlined), //add icon
+              childrenPadding:
+                  const EdgeInsets.only(left: 60), //children padding
+              children: [
+                ListTile(
+                  title: const Text("Bills"),
+                  onTap: () {
+                    //action on press
+                  },
+                ),
+
+                ListTile(
+                  title: const Text("Payments"),
+                  onTap: () {
+                    //action on press
+                  },
+                ),
+                ListTile(
+                  title: const Text("Purchases All"),
+                  onTap: () {
+                    //action on press
+                  },
+                ),
+                ListTile(
+                  title: const Text("PO"),
+                  onTap: () {
+                    //action on press
+                  },
+                ),
+                ListTile(
+                  title: const Text("Suppliers"),
+                  onTap: () {
+                    //action on press
+                  },
+                ),
+
+                //more child menu
+              ],
+            ),
+            ExpansionTile(
+              title: const Text("Inventory"),
+              leading: const Icon(Icons.inventory_outlined), //add icon
+              childrenPadding:
+                  const EdgeInsets.only(left: 60), //children padding
+              children: [
+                ListTile(
+                  title: const Text("Products"),
+                  onTap: () {
+                    //action on press
+                  },
+                ),
+
+                //more child menu
+              ],
+            ),
+            ListTile(
+              leading: const Icon(Icons.pie_chart),
+              title: const Text('Projects'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.report),
+              title: const Text('Reports'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.analytics),
+              title: const Text('Analytical Reports'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.token_outlined),
+              title: const Text('Generate Token'),
+              onTap: () {},
+            ),
+          ]),
+        ),
+        body: Center(
+          child: ElevatedButton(
+              onPressed: () {
+                showLoaderDialog(context);
+                _generateToken();
+              },
+              child: const Text('Get Token')),
+        ),
       ),
     );
   }
